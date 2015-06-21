d3622fed68179db2e1f43f970f4cdf8886c31a7c
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2015-06-21 20:44:21 +0700

    update url format and add support for format parameter

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,22 @@
-Router.route('/location', function () {
+Router.route('/locate/:format?', function () {
+  var format = this.params.format || 'json';
+  var formatToOutput = {
+    'json' : function(data){
+      return JSON.stringify(data);
+    },
+
+    'geogit' : function(data){
+      return [
+        'this commit comes from', 
+        data.country_name, ',',
+        data.city
+      ].join(' '); 
+    }
+  };
   var req = this.request;
   var res = this.response;
   var requestIp = req.headers['x-forwarded-for'];
   var geoData = FreeGeoIP.get(requestIp);
-  res.end(JSON.stringify(geoData.data));
+
+  res.end(formatToOutput[format].call(this,geoData.data));
 }, {where: 'server'});
