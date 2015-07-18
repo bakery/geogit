1cfbb9aaeb370e2be5bbb5c3f614cca746e47920
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2015-07-18 18:09:11 +0200

    clean up server code ----------------- [Geogit] // sent from France, Neuilly-sur-Seine [48.8833,2.2667] :FR:

--- geogit.js
+++ geogit.js
@@ -1,34 +1,5 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    //setInterval(function(){
-    console.log('trying geocode...');
-
-    IPGeocoder.geocode('128.101.101.101',function(err,geodata){
-      // log data :D
-      if(err){
-        console.error(err);
-      } else {
-        console.log(geodata);
-      }
-    });
-    //}, 5000);
+    IPGeocoder.load();
   });
 }
